04c9cd84a9b8bc5704a5d29f93d1f49a28cbc881
Pedro Nascimento <sn.jaypee@gmail.com>
2023-06-23 17:15:16 -0300

    Prject setup + add utils functions

---
+++ src/utils.js
@@ -0,0 +1,48 @@
+const users = [];
+const tweets = [];
+
+function addUser(username, avatar) {
+    const template = {
+        username,
+        avatar,
+    }
+    users.push(template);
+}
+
+function addTweet(username, tweet) {
+    const template = {
+        username,
+        tweet,
+    }
+    tweet.push(template);
+}
+
+function verifyUserExistence(username) {
+    return users.any(curr => curr.username === username);
+}
+
+function getTweetsUsingPage(pageId) {
+    const indexFall = (pageId - 1) * 10;
+    const startPoint = tweets.length - indexFall;
+    const finishPoint = (startPoint - 10) >= 0 ? (startPoint - 10) : 0; 
+
+    if(startPoint === 0) return [];
+
+    const pageTweets = [];
+    for (let index = startPoint; finishPoint > index; index--) {
+        const curr = tweets[index];
+        const user = users.find( user_ => user_.username === curr.username);
+        
+        const template = {
+            username: curr.username,
+            avatar: user.avatar,
+            tweet: curr.tweet,
+        }
+        
+        pageTweets.push(template);
+    }
+
+    return pageTweets;
+}
+
+export {getTweetsUsingPage, verifyUserExistence, addTweet, addUser};
